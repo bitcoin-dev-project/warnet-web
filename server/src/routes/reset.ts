5dea725d31dc0fea7ff9e583b4a2e2653df890a1
Emmanuel-Develops <itakpeemma@gmail.com>
2025-01-31 15:31:50 +0100

    debug(server): reset cache

--- server/src/routes/reset.ts
+++ server/src/routes/reset.ts
@@ -12,39 +12,54 @@ export const resetRoute = (app: Router) => {
   app.use("/reset", adminAuth, route);
 
   route.post("/", async (_req: Request, res: Response) => {
-    const isRunning = pollingService.isRunning();
-    if (isRunning) {
-      pollingService.stop();
-    }
-    const clearEvents = clearAllEvents();
-    if (clearEvents instanceof Error) {
+    try {
+      const isRunning = pollingService.isRunning();
+      if (isRunning) {
+        pollingService.stop();
+      }
+
+      try {
+        // reset team points
+        console.log("resetting team points")
+
+        const gameConfig = getGameConfig();
+  
+        if (gameConfig instanceof Error) {
+          return res.status(500).json({
+            message: gameConfig.message,
+            success: false,
+            data: null,
+          });
+        }
+  
+        const { teams } = gameConfig;
+        initializeTeamPoints(teams);
+      } catch (error: any) {
+        console.error("Error resetting team points:", error);
+        return res.status(500).json({
+          message: "Error resetting team points: " + error?.message,
+          success: false,
+          data: null,
+        });
+      }
+
+      // clear cache
+      console.log("clearing cache")
+      internalDataCache.reset();
+
+      return res.status(200).json({
+        message: "Database reset",
+        success: true,
+        data: null,
+      });
+
+    } catch (error: any) {
+      console.error("Error resetting", error);
       return res.status(500).json({
-        message: clearEvents.message,
+        message: error?.message ?? "Error resetting",
         success: false,
         data: null,
       });
     }
-
-    const gameConfig = getGameConfig();
-
-    if (gameConfig instanceof Error) {
-      return res.status(500).json({
-        message: gameConfig.message,
-        success: false,
-        data: null,
-      });
-    }
-
-    const { teams } = gameConfig;
-    initializeTeamPoints(teams);
-
-    // clear cache
-    internalDataCache.reset();
-
-    return res.status(200).json({
-      message: "Database reset",
-      success: true,
-      data: null,
-    });
   });
 }
